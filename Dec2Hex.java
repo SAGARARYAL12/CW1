6b13dd9f7962e0754195088b8e3d45b59f5bc28a
Ubuntu <ubuntu@ip-172-31-22-113.ec2.internal>
2024-10-26 11:52:53 +0000

    initial commit

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -2,46 +2,35 @@ import java.util.Scanner;
 
  
 
-class Dec2Hex 
+public class Dec2Hex {
+    
+    // Static method to convert a string input to hexadecimal
+    public static String convertToHex(String input) {
+        if (input == null || input.isEmpty()) {
+            return "Error: No input argument provided. Please enter an integer.";
+        }
 
-{ 
+        try {
+            int num = Integer.parseInt(input);
+            char[] ch = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+            StringBuilder hexadecimal = new StringBuilder();
 
-public static int Arg1; 
+            // Conversion logic
+            while (num != 0) {
+                int rem = num % 16;
+                hexadecimal.insert(0, ch[rem]);
+                num = num / 16;
+            }
 
-    public static void main(String args[])    { 
+            // Return statement indicating the hex representation
+            return "The Hex representation is: " + (hexadecimal.length() > 0 ? hexadecimal.toString() : "0");
+        } catch (NumberFormatException e) {
+            return "Error: Input is not a valid integer. Please enter a valid integer.";
+        }
+    }
 
-        Arg1 = Integer.parseInt(args[0]); 
-
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'}; 
-
-        int rem, num; 
-
-        num = Arg1; 
-
-        String hexadecimal=""; 
-
-        System.out.println("Converting the Decimal Value " + num + " to Hex..."); 
-
- 
-
-        while(num != 0) 
-
-        { 
-
-            rem=num%16; 
-
-            hexadecimal= ch[rem] + hexadecimal; 
-
-            num= num/16; 
-
-        } 
-
- 
-
-        System.out.println("Hexadecimal representation is: " + hexadecimal); 
-
- 
-
-    } 
-
-} 
+    public static void main(String[] args) {
+        String input = (args.length > 0) ? args[0] : "";
+        System.out.println(convertToHex(input));
+    }
+}
